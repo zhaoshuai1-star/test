6e641c56ac277ec0772b42dbd583d1c48f46c9b4
zhaoshuai1-star <85921275+zhaoshuai1-star@users.noreply.github.com>
2021-12-10 17:50:18 +0800

    添加新文件
    我试着从外面添加一个vue文件

---
+++ zhuangxiu.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>
+        <img src="../assets/详情信息.png" alt="">
+        <img style="float:left;margin-left:320px" src="../assets/房源详情2.png" alt="">
+    </div>  
+  </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
